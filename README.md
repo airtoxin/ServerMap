ccb84a7e1b68f2a826e1fb6b3ba979cf143fae99
airtoxin <airtoxin@icloud.com>
2015-04-09 06:42:23 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cartographer
+
+Resource monitoring tool for servers.
+
+## Developing
+
+### setup
+
+`$ virturlenv env && source venv/bin/activate`
+
+### packaging
+
+`$ python setup.py sdist`
---
+++ cartographer/cartographer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, with_statement
+
+class Cartographer(object):
+    """docstring for Cartographer"""
+    def __init__(self, arg):
+        super(Cartographer, self).__init__()
+        self.arg = arg
