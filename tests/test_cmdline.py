1c03c9c6d9a9b048828befa90243d09780038cdc
airtoxin <airtoxin@icloud.com>
2015-04-09 09:50:39 +0900

    Add test case

---
+++ tests/test_cmdline.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+from nose.tools import ok_, eq_
+from cartographer.cmdline import main
+
+class CmdlineTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_main(self):
+        eq_(main(), None)
