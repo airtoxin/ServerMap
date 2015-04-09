f136f3cd2599788fca01fb3fa3786c4679511d0b
airtoxin <airtoxin@icloud.com>
2015-04-09 14:13:06 +0900

    Add basic auth

--- cartographer/dashboard.py
+++ cartographer/dashboard.py
@@ -2,15 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, send_file
+from flask.ext.basicauth import BasicAuth
+from ConfigParser import SafeConfigParser
 from os import path
 
+conf = SafeConfigParser()
+conf.read("config.ini")
 sites_dir = path.join("..", "sites")
 
 app = Flask(__name__)
+
+if conf.getboolean("dashboard", "use_basic_auth"):
+    app.config["BASIC_AUTH_USERNAME"] = conf.get("dashboard", "username")
+    app.config["BASIC_AUTH_PASSWORD"] = conf.get("dashboard", "password")
+
+    basic_auth = BasicAuth(app)
 
 def run():
     app.run(debug=True)
 
 @app.route("/", methods=["GET"])
+@basic_auth.required
 def index():
     return send_file(path.join(sites_dir, "index.html"), mimetype="text/html")
