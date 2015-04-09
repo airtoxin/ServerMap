9529c7aaa48fe34885baa634d69b2b258e8c8622
airtoxin <airtoxin@icloud.com>
2015-04-10 00:37:37 +0900

    Add favicon

--- cartographer/dashboard.py
+++ cartographer/dashboard.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, send_file
+from flask import Flask, send_file, url_for
 from flask.ext.basicauth import BasicAuth
 from ConfigParser import SafeConfigParser
 from os import path
 
 conf = SafeConfigParser()
 conf.read("config.ini")
-sites_dir = path.join("..", "sites")
 
 app = Flask(__name__)
+static_dir = path.join(app.root_path, "..", "static")
 
 if conf.getboolean("dashboard", "use_basic_auth"):
     app.config["BASIC_AUTH_USERNAME"] = conf.get("dashboard", "username")
@@ -24,4 +24,8 @@ def run():
 @app.route("/", methods=["GET"])
 @basic_auth.required
 def index():
-    return send_file(path.join(sites_dir, "index.html"), mimetype="text/html")
+    return send_file(path.join(static_dir, "index.html"), mimetype="text/html")
+
+@app.route('/favicon.ico', methods=["GET"])
+def favicon():
+    return send_file(path.join(static_dir, "favicon.ico"), mimetype="image/vnd.microsoft.icon")
